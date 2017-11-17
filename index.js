9ece3d87b60b35682f5d9d511f426750ad30c7fa
Prasitha Thomas <prasitha.thomas@gmail.com>
2017-11-17 20:26:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance)
+{
+  if (distance <= 400)
+    {
+      return "This one is on me!"
+    }
+  if (distance > 2000)
+    {
+      return "I will gladly take your thirty bucks."
+    }
+}
