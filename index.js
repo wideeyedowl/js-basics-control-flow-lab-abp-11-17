15db7fa99685c925a1070aff03dcf0968f90343c
Prasitha Thomas <prasitha.thomas@gmail.com>
2017-11-17 20:27:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function scuberGreetingForFeet(distance)
     {
       return "I will gladly take your thirty bucks.";
     }
-  if (distance >2500)
+  if (distance > 2500)
     {
       return "No can do";
     }
