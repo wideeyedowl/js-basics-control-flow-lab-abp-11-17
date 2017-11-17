33b4ce7c87c20f057a85b1507dfe6d4fb1dae3e0
Prasitha Thomas <prasitha.thomas@gmail.com>
2017-11-17 20:42:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ function scuberGreetingForFeet(distance)
     {
       return "This one is on me!";
     }
-    else if (distance > 2500)
-    {
-      return "No can do";
-    }
     else if (distance > 2000 && distance < 2500)
       {
         return "I will gladly take your thirty bucks.";
       }
+    else if (distance > 2500)
+    {
+      return "No can do";
+    }
+    
 }
 
-function ternaryCheckCity(destination)
+function ternaryCheckCity(city)
 {
-  destination ==="NYC"? return "Ok, sounds good." : return "No go."
+  return (city ==="NYC") ? "Ok, sounds good." : "No go."
 }
